f0db4d08f39a591a4a410d34bb74781f7abc7e3a
Samer Odeh <81556567+Samerodeh@users.noreply.github.com>
2021-08-14 16:28:17 +0300

    Update test_stack_queue_animal_shelter.py

--- python/stack-queue-animal-shelter/tests/test_stack_queue_animal_shelter.py
+++ python/stack-queue-animal-shelter/tests/test_stack_queue_animal_shelter.py
@@ -1,15 +1,23 @@
 from stack_queue_animal_shelter import __version__
-
+from stack_queue_animal_shelter import * 
 
 def test_version():
     assert __version__ == '0.1.0'
 
 def test_enqueue():
-    expected = 'animal'
-    actual = 'Cats, Dogs'
-    assert expected is actual, "animal : Cats, Dogs"
+    actual = ('poppy', 'dog')
+    excepted = ('dog')
+    actual = ('jojo', 'cat')
+    excepted = ('cat')
+    actual = ('goldenfish', 'fish')
+    excepted = 'Not Cat or Dog'
+    assert actual == excepted
 
 def test_dequeue():
-    expected = 'name'
-    actual = 'pref'
-    assert expected is actual, "name : pref"
+    actual = ('maxi', 'dog')
+    excepted = ('dog')
+    actual = ('sela', 'cat')
+    excepted = ('cat')
+    actual = ('popo', 'parrot ')
+    excepted = 'Not Cat or Dog'
+    assert actual == excepted
