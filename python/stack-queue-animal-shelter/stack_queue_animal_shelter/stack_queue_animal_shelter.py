67bd763c7ef8619aaefba558431d780093181836
Samer Odeh <samerodeh220@gmail.com>
2021-08-14 16:23:52 +0300

    edit animal-shelter code challenge

--- python/stack-queue-animal-shelter/stack_queue_animal_shelter/stack_queue_animal_shelter.py
+++ python/stack-queue-animal-shelter/stack_queue_animal_shelter/stack_queue_animal_shelter.py
@@ -1,26 +1,63 @@
-Cat = []
-Dog = []
+class Node:
+  def __init__(self, value):
+    self.value = value
+    self.next = None
+
+class Queue():
+    def __init__(self):
+        self.head = None
+        self.Node = None
+
+    def enqueue(self, value):
+        node = Node(value)
+        if self.head == None:
+            self.head = node
+            self.Node = node
+            return self.Node.value
+        else:
+            self.Node.next = node
+            self.Node = node
+            return self.Node.value
+
+    def dequeue(self):
+            if self.head == None:
+              self.head = self.head.next
+              return self.head.value
+            else:
+              return None
+
+    def peek(self):
+            if self.head == None:
+              return self.head.value
+            else:
+              return None
+
+    def isEmpty(self):
+        return self.head == None
 
 class AnimalShelter:
-  def __init__(self):
-    self.cats = Cat()
-    self.dogs = Dog()
+    def __init__(self):
+        self.cat = Queue()
+        self.dog = Queue()
+        
+    def enqueue(self, animal, kind):
+        if kind == 'cat':
+            return self.cat.enqueue(animal)
+        elif kind == 'dog':
+            return self.dog.enqueue(animal)
+        else:
+            return 'Not Cat or Dog'
 
-  def enqueue(self,animal):
-    if animal.name == 'cats':
-      self.cats.enqueue(animal)
-    elif animal.name == 'dogs':
-      self.dogs.enqueue(animal)
-    else: 
-      return 'NOT Cat, NOT Dog'
+    def dequeue(self, pref):
+        if pref == 'dog':
+            return self.dog.dequeue()
+        if pref == 'cat':
+            return self.cat.dequeue()
+        else:
+            return 'Not Cat or Dog'
 
-  def dequeue(self, pref = None):
-    if pref == 'cats':
-      return self.cats.dequeue().name
-    elif pref == 'dogs':
-      return self.dogs.dequeue().name
-    else: 
-      return None 
+    def is_empty(self):
+        return None
 
 if __name__ == '__main__':
   pass
